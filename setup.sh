a4f5c8febbfc194c8de9bccf6635f751be1bb11f
shiraji <isogai.shiraji@gmail.com>
2018-04-09 15:49:20 +0900

    temporary shell script

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#1/bin/sh
+
+find . -not -name "*.pdf*"  -not -name "*.json*" -not -name "*.jpeg" -not -name "*.png" -not -name "*.js" -not -name "*.bcmap" -not -name "*.DS_Store" -not -name "*.html" -not -name "*.css" -not -name "LICENSE" -type f
